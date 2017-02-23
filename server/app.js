b1e07423697f91b4b5bfc51ab250f223c228de9b
Phil <PC@Stephanies-MacBook-Pro.local>
2017-02-22 20:23:52 -0600

    initial commit

---
+++ server/app.js
@@ -0,0 +1,1 @@
+console.log('Testing app.js to be initially sourced as a mock server');
---
+++ server/public/client.js
@@ -0,0 +1,5 @@
+console.log('Javascript Sourced');
+$(document).ready(function() {
+console.log('jQuery sourced');
+
+});// NOTE: FOR:           $(document).ready(function() {
