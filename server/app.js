5c6b4bb59daa523869f3258439c96daa3e021f15
Phil <PC@Stephanies-MacBook-Pro.local>
2017-02-22 22:23:01 -0600

    2 objects on console

--- server/app.js
+++ server/app.js
@@ -12,5 +12,17 @@ var app = express();
 // console.log('exampleApp: ', exampleApp); // results: exampleApp:  { name: 'Phil' }
 // Express is a function that will return a big object
 // NOTE: END Example of what 'var express = require('express');' AND 'var app = express();' is doing:
+
+var musicList = [
+  {name: 'Pink Floyd'},
+  {name: 'Grateful Dead'}
+]
+
 app.use(express.static('server/public'));
+app.get('/music', function(req, res){
+console.log('app.get(/music, function is logged: ', req);
+res.send(musicList);
+});// NOTE: FOR: app.get('/music', function(req, res){
+
+
 app.listen(5000);
--- server/public/client.js
+++ server/public/client.js
@@ -1,5 +1,16 @@
 console.log('Javascript Sourced');
 $(document).ready(function() {
 console.log('jQuery sourced');
+// NOTE:  Building dynamic file structure using Ajax
+$.ajax({
+type: 'GET',
+url: '/music',
+success: function(response) {
+console.log('success-function response: ', response); // logs the array of objects
 
-});// NOTE: FOR:           $(document).ready(function() {
+
+} // NOTE: FOR: success: function(response) {
+}); // NOTE: FOR: $.ajax({
+
+
+}); // NOTE: FOR:           $(document).ready(function() {
