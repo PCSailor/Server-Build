79c1c43fc15194f77e956722eb31e320e40b1fcc
Phil <PC@Stephanies-MacBook-Pro.local>
2017-02-22 21:46:57 -0600

    serving static files

--- server/app.js
+++ server/app.js
@@ -1,1 +1,16 @@
 console.log('Testing app.js to be initially sourced as a mock server');
+var express = require('express');
+var app = express();
+// TODO:  console.log(app);
+// NOTE:  Example of what 'var express = require('express');' AND 'var app = express();' is doing:
+// var taco = function() {
+// return {name: 'Phil'}
+// }
+// var exampleExpress = taco;
+// var exampleApp = taco();
+// console.log('exampleExpress: ', exampleExpress); // results: exampleExpress:  function () { return {name: 'Phil'} }
+// console.log('exampleApp: ', exampleApp); // results: exampleApp:  { name: 'Phil' }
+// Express is a function that will return a big object
+// NOTE: END Example of what 'var express = require('express');' AND 'var app = express();' is doing:
+app.use(express.static('server/public'));
+app.listen(5000);
